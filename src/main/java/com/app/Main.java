800659246e22a9a91f48c4243a7d598ee3ceb6e5
aelris <aelrismira@gmail.com>
2019-02-04 12:16:45 +0200

    improve Main.java

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -2,19 +2,29 @@ package com.app;
 
 import org.apache.avro.Schema;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        String schemaPath = args[0];
+        String csvPath = args[1];
+        String parquetPath =  args[2] + "_" + new Date().getTime() + ".parquet ";
+        int maxRows = Integer.parseInt(args[3]);
+
         //parsing avro schema
-        Schema schema = ParquetUtils.parseAvroSchema(args[0]);
+        Schema schema = ParquetUtils.parseAvroSchema(schemaPath);
 
         //converting csv(args[1]) to parquet file(args[2]) using avro schema(args[0])
-        ParquetUtils.writeToParquet(schema,args[1],args[2]);
+        ParquetUtils.writeToParquet(schema,csvPath,parquetPath);
 
         //printing content of generated parquet file(args[2]) with some maximum rows(args[3])
-        ParquetUtils.readParquetFile(args[2],Integer.parseInt(args[3]));
+        ParquetUtils.readParquetFile(parquetPath,maxRows);
 
     }
+
+
+
 
 }
